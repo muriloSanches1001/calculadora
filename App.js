dd40ed07e47903422b871f5053bfecc62b9afc08
aizelli <alexandre.izelli@gmail.com>
2024-05-16 09:09:42 -0300

    .

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import Button from './src/components/Button';
 import Display from './src/components/Display';
@@ -12,72 +12,80 @@ const initialState = {
 }
 
 export default function App() {
-  state = { ...initialState }
-  addDigit = n => {
-    const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
-    if (n === '.' && !clearDisplay && this.state.displayValue.includes('.')) {
-      return
+  const [state, setState] = useState(initialState);
+
+  const addDigit = n => {
+    const clearDisplay = state.displayValue === '0' || 
+    state.clearDisplay;
+    if (n === '.' && !clearDisplay && 
+    state.displayValue.includes('.')) {
+      return;
     }
-    
-    const currentValue = currentValue ? '' : this.state.displayValue
-    const displayValue = currentValue + n
-    this.setState({ displayValue, clearDisplay: false })
+
+    const currentValue = clearDisplay ? '' : state.displayValue;
+    const displayValue = currentValue + n;
+    setState({ ...state, displayValue, clearDisplay: false });
 
     if (n !== '.') {
-      const newValue = parseFloat(displayValue)
-      const values = [...this.state.values]
-      values[this.state.current] = newValue
-      this.setState({ values })
+      const newValue = parseFloat(displayValue);
+      const values = [...state.values];
+      values[state.current] = newValue;
+      setState({ ...state, values });
     }
   }
-  clearMemory = () => {
-    this.setState = ({ ...initialState })
+
+  const clearMemory = () => {
+    setState(initialState);
   }
-  setOperation = operation => {
-    if (this.state.current === 0) {
-      this.setState({ operation, current: 1, clearDisplay: true })
+
+  const setOperation = operation => {
+    if (state.current === 0) {
+      setState({ 
+        ...state, operation, current: 1, clearDisplay: true 
+      });
     } else {
-      const equals = operation === '='
-      const values = [...this.state.values]
+      const equals = operation === '=';
+      const values = [...state.values];
       try {
-        values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`)
-      }
-      catch (e) {
-        values[0] = this.state.values[0]
+        values[0] = eval(
+          `${values[0]} ${state.operation} ${values[1]}`
+        );
+      } catch (e) {
+        values[0] = state.values[0];
       }
 
-      values[1] = 0
-      this.setState({
+      values[1] = 0;
+      setState({
         displayValue: `${values[0]}`,
         operation: equals ? null : operation,
         current: equals ? 0 : 1,
         clearDisplay: !equals,
         values,
-      })
+      });
     }
   }
 
   return (
     <View style={styles.container}>
-      <Display value={this.state.displayValue} />
+      <Display value={state.displayValue} />
       <View style={styles.button}>
         <Button label='AC' triple onClick={clearMemory} />
-        <Button label='/' operation onClick={this.setOperation} />
+        <Button label='/' operation onClick={setOperation} />
         <Button label='7' onClick={addDigit} />
         <Button label='8' onClick={addDigit} />
         <Button label='9' onClick={addDigit} />
-        <Button label='*' operation onClick={this.setOperation} />
+        <Button label='*' operation onClick={setOperation} />
         <Button label='4' onClick={addDigit} />
         <Button label='5' onClick={addDigit} />
         <Button label='6' onClick={addDigit} />
-        <Button label='-' operation onClick={this.setOperation} />
+        <Button label='-' operation onClick={setOperation} />
         <Button label='1' onClick={addDigit} />
         <Button label='2' onClick={addDigit} />
         <Button label='3' onClick={addDigit} />
-        <Button label='+' operation onClick={this.setOperation} />
+        <Button label='+' operation onClick={setOperation} />
         <Button label='0' double onClick={addDigit} />
         <Button label='.' onClick={addDigit} />
-        <Button label='=' operation onClick={this.setOperation} />
+        <Button label='=' operation onClick={setOperation} />
       </View>
     </View>
   );
